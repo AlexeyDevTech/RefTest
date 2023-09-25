8a2dedcb038f57eef287b35b2def826184aee297
Alexey <cbyat0@mail.ru>
2023-09-25 15:55:47 +0300

    initial commit

---
+++ Ref.Console/Program.cs
@@ -0,0 +1,14 @@
+﻿using Ref;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var helper = new DeviceConnectHelper();
+        
+        
+        var device = helper.FindPort("R120#", "R120_OK", 115200);
+
+        Console.WriteLine($"device: {device?.port?.PortName ?? "<NULL>"}");
+    }
+}
---
+++ RefTest/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Prism.Mvvm;
+using Ref;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefTest
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private Device _refDevice;
+
+        public Device RefDevice
+        {
+            get => _refDevice;
+            set => SetProperty(ref _refDevice, value);
+        }
+
+        public MainWindowViewModel()
+        {
+            RefDevice = new Device();
+        }
+
+
+    }
+}
