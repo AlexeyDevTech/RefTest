935241456bcdbc3d0d54c43aa867cabaf5707bec
Alexey <cbyat0@mail.ru>
2023-10-02 11:56:35 +0300

    ы

--- Ref/RefLogic.cs
+++ Ref/RefLogic.cs
@@ -1,54 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Sockets;
-using System.Text;
-using System.Threading.Channels;
-using System.Threading.Tasks;
-
-namespace Ref
+﻿namespace Ref.Controllers.ReflectController
 {
     public class RefLogic
     {
         public RefLogic()
         {
-            
+
         }
     }
 
 
 
 
-    public class ReflectController120 : Controller
+    public class ReflectController : Controller
     {
-        public ReflectController120() : base()
+        public ReflectController() : base()
         {
 
         }
 
 
     }
-    public class ReflectController90 : Controller
-    {
-
-    }
-
-    public interface IReflectController
-    {
-        bool SetChannel(int channel);
-        bool SetMode(int mode);
-        bool SetImpulse(int impulse);
-        bool SetAmplitude(int vol);
-        void GetState();
-
-    }
 
     public class ReflectChannelSettings
     {
-        
-    }
-   
-   
-   
-}
 
+    }
+
+
+
+}
